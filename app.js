46e757cc44a2f9ff27baef6398db420fe4cc75bf
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-25 00:51:59 +0200

    Sending a response in the last middleware

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ app.use((req, res, next) => {
   console.log('In the middleware');
   next();
 });
-
 app.use((req, res, next) => {
   console.log('In another middleware');
+  res.send('<h1>Hello from express</h1>');
 });
-
 
 const server = http.createServer(app);
 
