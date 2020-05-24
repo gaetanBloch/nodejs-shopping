122ac8090506bdddb3d337b8b7d206085e2f0183
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-25 00:48:24 +0200

    Added Express middlewares

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+});
+
+
 const server = http.createServer(app);
 
 server.listen(3000);
