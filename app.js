589874bd78eccce2457b38d8ce5b3253fa5a6ce0
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-25 19:25:30 +0200

    Added body parsing middleware to parse the body sent from the form

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded());
 
 app.use('/add-product', (req, res, next) => {
   res.send('<form action="/product" method="post">' +
@@ -8,6 +11,11 @@ app.use('/add-product', (req, res, next) => {
     '<button type="submit">Add Product</button>' +
     '</form>');
 });
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express</h1>');
