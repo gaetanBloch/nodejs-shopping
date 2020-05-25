5e3c3dc64fb9f27e5e86ceb0b6dfe367b655c834
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-25 19:18:17 +0200

    Add a form and send a post request with the product title on submit

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In the middleware');
-  res.send('<h1>Add Product Page</h1>');
+  res.send('<form action="/product" method="post">' +
+    '<input type="text" name="title" placeholder="title">' +
+    '<button type="submit">Add Product</button>' +
+    '</form>');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In the middleware');
   res.send('<h1>Hello from express</h1>');
 });
 
