c437eec26728ff66076ba8183395f71eed7cfa29
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-28 22:29:39 +0200

    Added 404 Error Page

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404);
+  res.send('<h1 style="text-align: center">404 Page Not Found!</h1>');
+})
+
 app.listen(3000);
