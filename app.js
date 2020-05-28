6e92048d53f46d7b79c0d4e8f1a561199356c6f6
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-28 22:02:54 +0200

    Limited Middleware execution to POST Requests

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
   res.send('<form action="/product" method="post">' +
@@ -12,10 +12,10 @@ app.use('/add-product', (req, res, next) => {
     '</form>');
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
-})
+});
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express</h1>');
