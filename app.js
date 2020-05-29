f12904cc753985739fc46114bf06d4a630e2073e
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-29 19:33:10 +0200

    Fix path import

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
