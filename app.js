a7b58765bca9b955894ab326e69643f494202580
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-29 08:56:49 +0200

    Filtering Route Paths

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+// GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="post">' +
+  res.send('<form action="/admin/add-product" method="post">' +
     '<input type="text" name="title" placeholder="title">' +
     '<button type="submit">Add Product</button>' +
     '</form>');
 });
 
-router.post('/product', (req, res, next) => {
+// POST /admin/add-product
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
