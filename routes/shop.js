5a17481959d1e7c5249b54defe540eb28f6c84a1
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-29 10:58:31 +0200

    Serve the shop.html page when the '/' route gets loaded

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from express</h1>');
+  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
