88b4d7802a82ac89109f5938d1c4fccbf1376d75
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-30 15:05:32 +0200

    Use the main-layout for the not-found page

--- routes/not-found.js
+++ routes/not-found.js
@@ -6,7 +6,7 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.status(404).render('not-found');
+  res.status(404).render('not-found', { title: 'Page Not Found' });
 });
 
 module.exports = router;
