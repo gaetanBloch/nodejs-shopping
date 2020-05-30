b3dd34637a38344a4653e466430541b76c66bc73
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-30 10:48:26 +0200

    Use pug template engine with express

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const notFoundRoutes = require('./routes/not-found');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
