894e6481c8de66bd9fba0fa76992a5112d4e2d1e
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 23:46:21 +0200

    Pass the product ID with POST request to add to Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -37,6 +37,11 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  console.log(req.body.productId);
+  res.redirect('/cart');
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     title: 'Your Orders',
