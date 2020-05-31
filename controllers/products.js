c366d3f7ea7fea61752622a245174f72126c8a96
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 13:27:53 +0200

    Use the product model in the products controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -8,13 +8,14 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
   res.render('shop', {
-    products,
+    products: Product.fetchAll(),
     title: 'Shop',
     path: '/',
   });
