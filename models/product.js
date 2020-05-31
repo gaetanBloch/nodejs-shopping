8931429074fa5ad9788fc771317f6a6ba23b9cb9
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 16:20:36 +0200

    Read the products from the file

--- models/product.js
+++ models/product.js
@@ -1,37 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = [];
+const productsFile = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
-  save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        // If there is no error then the file exists and we can read the
-        // products
-        products = JSON.parse(fileContent);
-      }
+  save = () => {
+    fs.readFile(productsFile, (err, fileContent) => {
+      const products = Product.readProducts(err, fileContent);
       products.push(this);
       // Persists the products to the file
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(productsFile, JSON.stringify(products), (err) => {
         if (err) {
           console.log(err);
         }
       });
     });
-  }
+  };
 
-  static fetchAll() {
-    return products;
-  }
+  static readProducts = (err, fileContent) => {
+    if (!err) {
+      // If there is no error then the file exists and we can read the
+      // products
+      return JSON.parse(fileContent);
+    }
+    return [];
+  };
+
+  static fetchAll = () => {
+    fs.readFile(productsFile, (err, fileContent) => {
+      return Product.readProducts(err, fileContent);
+    });
+  };
 };
