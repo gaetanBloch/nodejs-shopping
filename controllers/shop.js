9b6fe40f552ea82ab849890e9f1466683e97ebdc
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 22:53:38 +0200

    Extracting Dynamic parameter product id

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,6 +20,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId;
+  console.log(productId);
+  res.redirect('/');
+};
+
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     title: 'Your Cart',
--- routes/shop.js
+++ routes/shop.js
@@ -8,6 +8,8 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
