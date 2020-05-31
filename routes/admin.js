0324d1729d9c0bc82a70e71a2201d2d117d787eb
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 12:01:22 +0200

    Removed unused utils function

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path');
-
 const router = express.Router();
 
 const products = [];
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path');
 const adminData = require('../routes/admin');
 
 const router = express.Router();
