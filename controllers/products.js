282d264c379e4a7a8052c1d30e457937509036e7
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 17:07:18 +0200

    Move the add product page to admin directory

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     title: 'Add Product',
     path: '/admin/add-product',
   });
