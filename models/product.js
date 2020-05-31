70a53ef2f0d754cb659f5de4f11147be54aabb77
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 14:43:51 +0200

    Read the products.json file to extract existing products

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
 
 module.exports = class Product {
@@ -6,7 +9,16 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(process.mainModule.filename, 'data', 'products.json')
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+    });
   }
 
   static fetchAll() {
