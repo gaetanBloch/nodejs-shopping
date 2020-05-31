2d990611cf3dcbd6cc4d5879603422ff05b629ce
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 11:59:21 +0200

    Fix path value to null for page not found

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).render('not-found', { title: 'Page Not Found' });
+  res.status(404).render('not-found', { title: 'Page Not Found', path: null });
 });
 
 app.listen(3000);
