53b681d410dedeb9e0a2d18db78f9b52ee1653c6
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-31 12:58:34 +0200

    Externalize the getProducts fucntion in the controller and use it in the shop page

--- controllers/products.js
+++ controllers/products.js
@@ -3,11 +3,19 @@ const products = [];
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
     title: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
   });
-}
+};
 
 exports.postAddProduct = (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect('/');
-}
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render('shop', {
+    products,
+    title: 'Shop',
+    path: '/',
+  });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,9 @@
 const express = require('express');
 
-const adminData = require('../routes/admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('shop', {
-    products: adminData.products,
-    title: 'Shop',
-    path: '/'
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
