430e1c5989002f3d0ef96101593a3c4d8d95b0c4
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-02 14:50:28 +0200

    Change database config

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('nodejs-shopping', 'root', 'gaetan.bloch', {
+const sequelize = new Sequelize('nodejs-shopping', 'root', '', {
   dialect: 'mysql',
   host: 'localhost'
 });
