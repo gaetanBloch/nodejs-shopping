a20bc2fbd020d5cf33dc4b1fb934c3194f40c361
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-02 23:47:09 +0200

    Set up Realtions between Users, Orders and OrderItems

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -40,6 +42,10 @@ Cart.belongsTo(User);
 User.hasOne(Cart);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
+Product.belongsToMany(Order, {through: OrderItem});
 
 sequelize
   // .sync({ force: true })
