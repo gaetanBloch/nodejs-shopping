3e693f9cac6c23ef08d726e736c5fd7878a9f4e3
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-02 13:24:38 +0200

    Connecting to the MySQL DB via sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'gaetan.bloch',
-  database: 'nodejs-shopping'
+const sequelize = new Sequelize('nodejs-shopping', 'root', 'gaetan.bloch', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
