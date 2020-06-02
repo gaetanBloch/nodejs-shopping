b2ad3603b6f948876f807374727943a97b9e9a94
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-02 23:44:42 +0200

    Added Order and OrderItem Models

---
+++ models/order-item.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = require('../utils/database');
+
+const OrderItem = sequelize.define('orderItem', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  quantity: Sequelize.INTEGER
+});
+
+module.exports = OrderItem;
---
+++ models/order.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = require('../utils/database');
+
+const Order = sequelize.define('order', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  }
+});
+
+module.exports = Order;
