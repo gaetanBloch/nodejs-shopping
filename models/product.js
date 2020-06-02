a18286a90e0046932eaf2e53d68dc2a6413b8c49
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-02 12:51:55 +0200

    Added save method to do INSERT request in the DB

--- models/product.js
+++ models/product.js
@@ -11,7 +11,13 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save = () => {};
+  save = () => {
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) ' + 
+      'VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  };
 
   static fetchAll = () => {
     return db.execute('SELECT * FROM products');
