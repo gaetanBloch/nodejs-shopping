ac37cc075feb8d1416acad110eb19a5549b8fcab
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-02 14:14:19 +0200

    Change the way we import sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('nodejs-shopping', 'root', 'gaetan.bloch', {
   dialect: 'mysql',
