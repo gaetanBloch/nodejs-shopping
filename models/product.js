ff43821b7ae199ca57c0868b94ee21df6470aa17
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-03 19:06:53 +0200

    Created fetchAll method in Product Model

--- models/product.js
+++ models/product.js
@@ -16,6 +16,19 @@ class Product {
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
   }
+
+  static fetchAll = () => {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => console.log(err));
+  };
 }
 
 module.exports = Product;
