a693da9feea8e8363ba02be30c9c867d045e0288
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-03 22:47:14 +0200

    Bug fix in creating new Product

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
-    this._id = new mongodb.ObjectID(_id);
+    this._id = _id ? new mongodb.ObjectID(_id) : null;
   }
 
   save() {
