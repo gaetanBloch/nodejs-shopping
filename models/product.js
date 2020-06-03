4ee77c28c8809b0e2ad1e748462b6c014eaf20b4
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-03 22:20:55 +0200

    Possibility to update or save a product in the Product Model

--- models/product.js
+++ models/product.js
@@ -2,18 +2,28 @@ const mongodb = require('mongodb');
 const { getDb } = require('../utils/database');
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, _id = null) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOperation;
+    if (this._id) {
+      // Save existing product
+      dbOperation = db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectID(this._id) }, { $set: this });
+    } else {
+      // Create new product
+      dbOperation = db.collection('products').insertOne(this);
+    }
+
+    return dbOperation
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
   }
