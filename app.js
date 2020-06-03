9331db73d4770b09b2c09e99d4f0383019a5a378
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-03 14:30:48 +0200

    Changed the product Model

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   //   .catch((err) => console.log(err));
 });
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 app.use(errorController.getNotFound);
 
--- models/product.js
+++ models/product.js
@@ -1,6 +1,17 @@
-const { Sequelize } = require('sequelize');
+const mongoConnect = require('../utils/database');
 
-const sequelize = require('../utils/database');
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save() {
+
+  }
+}
 
 const Product = sequelize.define('product', {
   id: {
