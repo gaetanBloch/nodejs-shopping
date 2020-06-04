1c294bc37fa33031df976ec50a7f111d2c5d2889
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-04 17:56:13 +0200

    Added addToCart method in User Model

--- models/user.js
+++ models/user.js
@@ -26,4 +26,20 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// Use the function keyword so that the "this" keyword refers to the context by
+// which is it executed. No arrow function!
+userSchema.methods.addToCart = function (product) {
+  const products = [...this.cart.products];
+  const productIndex = products.findIndex(
+    (prod) => prod.productId.toString() === product._id.toString()
+  );
+  if (productIndex <= -1) {
+    products.push({ productId: product._id, quantity: 1 });
+  } else {
+    products[productIndex].quantity = products[productIndex].quantity + 1;
+  }
+  this.cart = { products };
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
