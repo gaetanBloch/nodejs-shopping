f58acde9b644b31ff796b50bc7477b975ea53c1b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-05 17:30:41 +0200

    Added express-session dependency

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,12 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[3]
-    .trim()
-    .split('=')[1] == 'true';
   res.render('auth/login', {
     title: 'Login',
     path: '/login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: true
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=100000; HttpOnly');
+  res.setHeader('Set-Cookie','');
   res.redirect('/');
 };
