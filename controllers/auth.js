e141a60fdaf2701675e961b47e30e6ac8088e1b8
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-05 15:59:17 +0200

    Setting and getting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').split(';')[3].trim().split('=')[1];
   res.render('auth/login', {
     title: 'Login',
     path: '/login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true');
   res.redirect('/');
 };
