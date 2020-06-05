7e522e9272d8cde7e0362cd99f9d25ea3271d2e3
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-05 17:51:26 +0200

    Used MongoDB to Store Sessions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDbSessionStore = require('connect-mongodb-session')(session);
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -11,7 +12,14 @@ const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://gbloch:gaetan.bloch@' +
+  'cluster0-hcscb.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDbSessionStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'my secret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store
 }));
 
 app.use((req, res, next) => {
@@ -40,11 +49,7 @@ app.use(authRoutes);
 app.use(errorController.getNotFound);
 
 mongoose
-  .connect(
-    'mongodb+srv://gbloch:gaetan.bloch@cluster0-hcscb.mongodb.net' +
-    '/shop?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Successfully connected to MongoDb!');
     User.findOne().then((user) => {
