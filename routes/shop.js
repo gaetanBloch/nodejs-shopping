5702a1904642c94c0e99689a210621e19eb7975c
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-06 21:55:53 +0200

    Protect the Cart and Orders routes with the Auth Middleware

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -10,14 +11,14 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.post('/cart/delete-item', shopController.postCartDeleteProduct);
+router.post('/cart/delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.get('/create-order', shopController.postOrder);
+router.get('/create-order', isAuth, shopController.postOrder);
 
-router.get('/orders', shopController.getOrders);
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
