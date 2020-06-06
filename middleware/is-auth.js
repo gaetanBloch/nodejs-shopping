62d47faf79893567779f0369f75eabbaf068b18c
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-06 22:02:07 +0200

    Fixed is-auth middleware

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-  if (!res.session.isLoggedIn) {
-    return res.redirect('login');
+  if (!req.session.isLoggedIn) {
+    return res.redirect('/login');
   }
   next();
 }
