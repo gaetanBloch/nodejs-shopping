2e43b27e1588fb487c07d3d5d1162a82d1e0029e
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-06 08:57:28 +0200

    Added GET and POST Signup Routes

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,9 @@ router.get('/login', authController.getLogin);
 
 router.post('/login', authController.postLogin);
 
-router.get('/logout', authController.getLogout);
+router.get('/signup', authController.getSignup);
+
+router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
