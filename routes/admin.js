c2198b2e16b69c2a7907cda517e13c8a4170e74a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-07 23:15:18 +0200

    Added Validation For the add product Page

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
@@ -9,7 +10,17 @@ const router = express.Router();
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // POST /admin/add-product
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post(
+  '/add-product',
+  [
+    body('title').isAlphanumeric().isLength({ min: 3 }).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 5, max: 400 }).trim()
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
 
 // GET /admin/products
 router.get('/products', isAuth, adminController.getProducts);
