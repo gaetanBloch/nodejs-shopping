671f21b85b40a22c3867dd610fd33e33325623e5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-07 09:13:19 +0200

    Configured nodemailer transporter

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key : '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'
+  }
+}))
 
 const { getErrorMessage } = require('./utils');
 
