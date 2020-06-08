f82e86b93711ef5c8fdccbf988620539f208b082
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-08 22:43:06 +0200

    Download Order invoice file

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -92,4 +95,16 @@ exports.getOrders = (req, res, next) => {
       });
     })
     .catch(err => forwardError(err, next));
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      next(err);
+    }
+    res.send(data);
+  })
 };
