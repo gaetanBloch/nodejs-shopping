257aaf2bbd39b3f720bb4a428bca0df894a8eb50
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-08 21:57:17 +0200

    Serving the Product images statically

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(session({
   secret: 'my secret',
   resave: false,
