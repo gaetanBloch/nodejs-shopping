24f8f440ff97e52d2bf5932b6f73c7f9b0568920
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-09 23:07:32 +0200

    Add Checkout Success Route

--- routes/shop.js
+++ routes/shop.js
@@ -19,6 +19,8 @@ router.post('/cart/delete-item', isAuth, shopController.postCartDeleteProduct);
 
 router.get('/checkout', isAuth, shopController.getCheckout);
 
+router.get('/checkout/success', isAuth, shopController.getCheckoutSuccess);
+
 router.post('/create-order', isAuth, shopController.postOrder);
 
 router.get('/orders', isAuth, shopController.getOrders);
