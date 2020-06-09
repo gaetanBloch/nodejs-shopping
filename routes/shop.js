51cabcecdc65fb08a778531f7f5a2b3e5644b2b5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-09 23:08:07 +0200

    Add Checkout Cancel Route

--- routes/shop.js
+++ routes/shop.js
@@ -21,6 +21,8 @@ router.get('/checkout', isAuth, shopController.getCheckout);
 
 router.get('/checkout/success', isAuth, shopController.getCheckoutSuccess);
 
+router.get('/checkout/cancel', isAuth, shopController.getCheckoutCancel);
+
 router.post('/create-order', isAuth, shopController.postOrder);
 
 router.get('/orders', isAuth, shopController.getOrders);
