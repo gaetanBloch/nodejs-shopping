d8751ef45f8ea6227c602ed0f15785dc814ddf4d
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-09 19:11:54 +0200

    Remove the product from the DOM without reloading the page

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,9 +2,17 @@ const deleteProduct = (button) => {
   const productId = button.parentNode.querySelector('[name=id]').value;
   const csrf = button.parentNode.querySelector('[name=_csrf]').value;
 
+  const productElement = button.closest('article');
+
   fetch('/admin/product/' + productId, {
     method: 'DELETE',
     headers: { 'csrf-token': csrf }
-  }).then(response => console.log(response))
+  }).then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // To Support IE
+      // productElement.parentNode.removeChild(productElement);
+      productElement.remove();
+    })
     .catch(err => console.log(err));
 };
