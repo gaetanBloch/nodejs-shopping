31fa6e673b981a663cdbfe146423cf1a07cceb92
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-06-09 22:12:34 +0200

    Set up a Route for the Checkout Page

--- routes/shop.js
+++ routes/shop.js
@@ -17,6 +17,8 @@ router.post('/cart', isAuth, shopController.postCart);
 
 router.post('/cart/delete-item', isAuth, shopController.postCartDeleteProduct);
 
+router.get('/checkout', isAuth, shopController.getCheckout);
+
 router.post('/create-order', isAuth, shopController.postOrder);
 
 router.get('/orders', isAuth, shopController.getOrders);
